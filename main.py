7949af58af42c20a79a10709a1f1886e8fbd3b7d
Wei Rong <56021409+wr1159@users.noreply.github.com>
2022-10-16 13:37:51 +0000

    MVP, added cancel and leaderboard feature.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import random
 import telebot
+from telebot.async_telebot import AsyncTeleBot
 from telebot import types,util
 from flask import Flask
 from threading import Thread
 from dbhelper import DBHelper
+import time
+from replit import db
 
 
 '''
@@ -30,6 +33,7 @@ if no: update timestamp -> temporary value V reset to 1
 '''
 
 API_KEY = os.getenv('API_KEY') 
+TOKEN = os.getenv('TOKEN')
 PORT = int(os.environ.get('PORT', 5000))
 
 app = Flask('')
@@ -47,21 +51,69 @@ def keep_alive():
     t.start()
   
 keep_alive()
+# bot = telebot.TeleBot(API_KEY)
+bot = telebot.TeleBot(TOKEN)
+# bot = AsyncTeleBot(API_KEY)
 
-db = DBHelper()
-db.setup()
-bot = telebot.TeleBot(API_KEY)
-bot.delete_webhook()
-
-
+sent_poll = ""
+target = ""
+count = 0
 @bot.message_handler(commands=['cancel'])
 def cancel(message):
-  print("Cancel " + message)
+  global target
+  global count
+  global sent_poll
+  target = message.text[message.entities[1].offset:]
+  if target in db.keys():
+    count = db[target]
+  sent_poll = bot.send_poll(chat_id= message.chat.id,
+    question= "do we cancel " + target,
+    options= ("Yes", "No"),
+    is_anonymous= False,
+    open_period = 10)
+  print(message.text[message.entities[1].offset:])
+  print("Cancel " + str(message.entities[0]))  
+  
+@bot.poll_handler(func = None)
+def poll_stop(poll):
+  print(poll)
+  # if(poll.is_closed):
+  options = poll.options
+  print(options[0].voter_count)
+  print(options[1].voter_count)
+  print(target)
+  if(options[0].voter_count > options[1].voter_count):
+    if(target not in db.keys()):
+      db[target] = count
+    else:
+      db[target] = count + 1
+  
+  
   
 @bot.message_handler(commands=['leaderboard'])
 def leaderboard(message):
+  print(message.chat.id)
+  keys = db.keys()
+  cancelled = list()
+  for key in keys:
+    cancelled.append(str(db[key]) + "  " + key)
+  cancelled.sort()
+  cancelled.reverse()
+  output_message = ""
+  for i in range(len(cancelled)):
+    output_message += str(i+1) + ". "
+    output_message += cancelled[i][2:] + ":  " + cancelled[i][:1] 
+    output_message += "\n"
+  if output_message == "":
+    output_message = "leaderboard is empty as of now."
+  bot.send_message(chat_id=message.chat.id, text= output_message)
   print("Leaderboard called")
-  
+
+# import asyncio
+# asyncio.run(bot.polling())
+bot.infinity_polling()
+
+#code to model after
 # @bot.message_handler(commands=['praiseyou'])
 # def praiseyou(message):
 #   markup = types.ForceReply(selective=False)
